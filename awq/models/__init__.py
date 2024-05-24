2f541b4f58af91c3eab58c6c2021993c7fa62fe1
apt-team <code.x.0018@gmail.com>
2024-05-24 14:32:53 +0530

    finxed issues

--- awq/models/__init__.py
+++ awq/models/__init__.py
@@ -17,3 +17,5 @@ from .qwen2 import Qwen2AWQForCausalLM
 from .gemma import GemmaAWQForCausalLM
 from .stablelm import StableLmAWQForCausalLM
 from .starcoder2 import Starcoder2AWQForCausalLM
+from .cohere import CohereAWQForCausalLM
+from .phi3 import Phi3AWQForCausalLM
